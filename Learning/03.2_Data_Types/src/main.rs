8b285d71a28ae27f916c03f6aa291a4a4163b9c4
Shrey Vats <137366687+Shrey-Vats@users.noreply.github.com>
2025-09-29 23:05:17 +0530

    Learn data types in rust

---
+++ Learning/03.2_Data_Types/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let guess: u32 = "44".parse().expect("Error on changing number type");
+}
+
+fn scalar_Types() {
+
+// Length	Signed	Unsigned
+// 8-bit	i8	u8
+// 16-bit	i16	u16
+// 32-bit	i32	u32
+// 64-bit	i64	u64
+// 128-bit	i128	u128
+// architecture dependent	isize	usize
+
+let a 
+}
